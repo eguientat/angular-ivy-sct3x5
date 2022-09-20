9364f537275fc85279c402cdd72d4298e012d278
eguientat <62184912+eguientat@users.noreply.github.com>
2022-09-20 11:55:16 +0200

    2 files modified

--- src/app/pizza-details/pizza-details.component.ts
+++ src/app/pizza-details/pizza-details.component.ts
@@ -3,7 +3,6 @@ import { Pizza } from '../pizza';
 
 @Component({
   selector: 'app-pizza-details',
-  template: `<h1>Hello {{pp.name}}!</h1>`,
   templateUrl: './pizza-details.component.html',
   styleUrls: ['./pizza-details.component.css'],
 })
