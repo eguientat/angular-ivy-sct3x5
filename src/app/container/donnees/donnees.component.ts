f05cf84f7750a426dd7f341c681801bff1dc10b9
eguientat <62184912+eguientat@users.noreply.github.com>
2022-10-04 09:36:17 +0200

    4 files modified

--- src/app/container/donnees/donnees.component.ts
+++ src/app/container/donnees/donnees.component.ts
@@ -7,11 +7,25 @@ import { Pizza } from '../../shared/pizza';
   styleUrls: ['./donnees.component.css'],
 })
 export class DonneesComponent implements OnInit {
-  CurrentPizza = new Pizza(
-    'marguerite',
-    '../../assets/margarita.jpg',
-    "La pizza Margherita est le nom d'une spécialité culinaire traditionnelle de la ville de Naples en Italie. "
-  );
+  name = 'Oui';
+
+  public pizza1: Pizza[] = [
+    new Pizza(
+      'marguerite',
+      '../../assets/margarita.jpg',
+      "La pizza Margherita est le nom d'une spécialité culinaire traditionnelle de la ville de Naples en Italie. "
+    ),
+    new Pizza(
+      'Mex',
+      '../../assets/mex.jpg',
+      'a pizza Tex-Mex, un savant mélange de ...'
+    ),
+    new Pizza(
+      '4 Fromages',
+      '../../assets/fromage.jpg',
+      "Pizza quattro formaggi Italian: est une variété de pizza dans la cuisine italienne qui est garnie d'une combinaison de quatre types de fromage"
+    ),
+  ];
 
   constructor() {}
 
--- src/app/container/list/pizza-list/pizza-list.component.ts
+++ src/app/container/list/pizza-list/pizza-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Pizza } from '../../../shared/pizza';
 
 @Component({
@@ -7,23 +7,9 @@ import { Pizza } from '../../../shared/pizza';
   styleUrls: ['./pizza-list.component.css'],
 })
 export class PizzaListComponent implements OnInit {
-  @Input() public pizza: Pizza[] = [
-    new Pizza(
-      'marguerite',
-      '../../assets/margarita.jpg',
-      "La pizza Margherita est le nom d'une spécialité culinaire traditionnelle de la ville de Naples en Italie. "
-    ),
-    new Pizza(
-      'Mex',
-      '../../assets/mex.jpg',
-      'a pizza Tex-Mex, un savant mélange de ...'
-    ),
-    new Pizza(
-      '4 Fromages',
-      '../../assets/fromage.jpg',
-      "Pizza quattro formaggi Italian: est une variété de pizza dans la cuisine italienne qui est garnie d'une combinaison de quatre types de fromage"
-    ),
-  ];
+  @Input() pizza: Pizza[];
+  @Input() pp : string;
+
   constructor() {}
 
   ngOnInit() {}
